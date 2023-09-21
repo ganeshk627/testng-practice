42e49ab0e65da5e9daf078217a1a50cf63351d09
Ganesh K <ganeshkannan627@gmail.com>
2023-09-21 10:03:05 +0530

    Adding include and exclude

---
+++ src/test/java/tests/IncludeExclude.java
@@ -0,0 +1,38 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import pageobjects.DashboardPage;
+import pageobjects.LoginPage;
+
+import java.time.Duration;
+
+public class IncludeExclude {
+
+
+
+    @Test
+    public void Test1() {
+
+        System.out.println("Test1");
+    }
+
+    @Test
+    public void Test2() {
+        System.out.println("Test2");
+
+    }
+
+    @Test
+    public void Test3() {
+        System.out.println("Test3");
+
+    }
+
+    @Test
+    public void Test4() {
+        System.out.println("Test4");
+
+    }
+}
