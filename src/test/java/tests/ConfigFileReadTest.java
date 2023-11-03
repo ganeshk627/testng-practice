38e57ccbb047b0a9e811b34b1334ff229dd33d02
Ganesh K <ganeshkannan627@gmail.com>
2023-11-03 09:52:19 +0530

    updating properties

--- src/test/java/tests/ConfigFileReadTest.java
+++ src/test/java/tests/ConfigFileReadTest.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -22,19 +23,28 @@ public class ConfigFileReadTest {
     @Test
     public void configFromFile() throws IOException {
 
-
+        // reading config file
         FileInputStream fis = new FileInputStream("src/test/resources/config.properties");
+//        FileInputStream env = new FileInputStream("src/test/resources/env.properties");
 
-
+        // loading properties to 'properties' object
         Properties properties = new Properties();
         properties.load(fis);
+//        properties.load(env);
 
+        // reading and writing property values
         System.out.println(properties.getProperty("url"));
         System.out.println(properties.getProperty("username.admin"));
         System.out.println(properties.getProperty("password.admin"));
         System.out.println(properties.getProperty("password.user"));
+        properties.setProperty("username.user", "ganeshk");
         properties.setProperty("password.user", "user@123");
         System.out.println(properties.getProperty("password.user"));
+
+
+        // saving all the properties to property file
+        FileOutputStream fos = new FileOutputStream("src/test/resources/config1.properties");
+        properties.store(fos, null);
 
     }
 }
